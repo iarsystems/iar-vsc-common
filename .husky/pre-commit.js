31cd91924520988b02068e9da04f28a0e93056ca
Hampus Adolfsson <hampus.adolfsson@iar.com>
2022-09-08 12:23:33 +0200

    Avoid checking for MPL headers in generated files

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -2,7 +2,6 @@
 // This hook forces all source files in the repository to have the MPL license header at the start of the file.
 /// -----------------------------
 
-const { spawnSync } = require("child_process");
 const { readFileSync } = require("fs");
 const { exit } = require("process");
 var proc = require('child_process');
@@ -20,7 +19,9 @@ const MPLv2_header = `/* This Source Code Form is subject to the terms of the Mo
 runAndGetStdout("git diff --cached --name-only --diff-filter=ACMR", function(changedFiles) {
     const filesArray = changedFiles.trim().split("\n");
     // Check that all typescript files have the mpl preamble
-    const res = filesArray.filter(file => file.endsWith(".ts")).map(file => {
+    const res = filesArray.filter(file => file.endsWith(".ts"))
+        .filter(file => !/src\/thrift\/bindings\/.*/.test(file))
+        .map(file => {
         const contents = readFileSync(file).toString().replace(/\r\n/g, "\n");
         if (!contents.startsWith(MPLv2_header)) {
             console.log("Missing or malformed MPLv2 license header in file: " + file);
